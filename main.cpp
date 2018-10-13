6e5cc70811e3580f2b63b4ba2d486d9a0a83053b
AntoninLoubiere <antonin.loubiere@laposte.net>
2018-10-13 18:04:10 +0200

    Resolve bug (issue #2): add translator

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
+#include "status_getter.h"
 #include "main_window.h"
+
 #include <QApplication>
-
-#include <QDebug>
-
-#include <status_getter.h>
+#include <QLocale>
+#include <QTranslator>
+#include <QLibraryInfo>
 
 #ifdef Q_OS_UNIX
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
+
+    // set translator
+    QString locale = QLocale::system().name().section('_', 0, 0);
+    QTranslator translator;
+    translator.load(QString("qt_" + locale), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    app.installTranslator(&translator);
+
     MainWindow w;
     w.show();
 
-
-
-    return a.exec();
+    return app.exec();
 }
 #endif
